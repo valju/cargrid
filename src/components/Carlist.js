1bc522549f7facef35c331affde625e1451263c6
juhahinkula <hinkula@gmail.com>
2020-04-15 12:44:47 +0300

    grid modifications

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,20 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 export default function Carlist() {
   const [cars, setCars] = useState([]);
-  const [columns, setColumns] = useState([
-    {
-      headerName: 'Brand',
-      field: 'brand'
-    },
-    {
-      headerName: 'Model',
-      field: 'model'
-    }
-  ]);
 
   useEffect(() => {
     getCars();
@@ -27,9 +17,48 @@ export default function Carlist() {
     .catch(err => console.error(err))
   }
 
+  const columns = [
+    {
+      headerName: 'Brand',
+      field: 'brand',
+      sortable: true,
+      filter: 'agTextColumnFilter',
+    },
+    {
+      headerName: 'Model',
+      field: 'model',
+      sortable: true,
+      filter: 'agTextColumnFilter',
+    },
+    {
+      headerName: 'Color',
+      field: 'color',
+      sortable: true,
+      filter: 'agTextColumnFilter',
+    },
+    {
+      headerName: 'Fuel',
+      field: 'fuel',
+      sortable: true,
+      filter: 'agTextColumnFilter',
+    },
+    {
+      headerName: 'Year',
+      field: 'year',
+      sortable: true,
+      filter: 'agNumberColumnFilter',
+    },
+    {
+      headerName: 'Price',
+      field: 'price',
+      sortable: true,
+      filter: 'agNumberColumnFilter',
+    }
+  ]
+
   return(
-    <div className="ag-theme-balham" style={ {height: '400px', width: '1000px'} }>
-      <AgGridReact columnDefs={columns} rowData={cars}>
+    <div className="ag-theme-material" style={{height: '700px', width: '100%'}}>
+      <AgGridReact floatingFilter={true} pagination={true} paginationAutoPageSize={true} columnDefs={columns} rowData={cars}>
       </AgGridReact>
     </div>
   );
