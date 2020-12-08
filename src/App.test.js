b13ca21bd81d819390a26dab65c58fc4aabb6251
juhahinkula <hinkula@gmail.com>
2020-12-08 15:37:52 +0200

    Unit test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
+import Carlist from './components/Carlist';
 
 test('App.js renders', () => {
   const { getByText } = render(<App />);
   const element = getByText(/CarList/i);
+  expect(element).toBeInTheDocument();
+});
+
+test('New Car button opens modal form', () => {
+  const { getByText } = render(<Carlist />);
+  const button = getByText(/NEW CAR/i);
+  fireEvent.click(button);
+  const element = getByText(/Add Car/i)
   expect(element).toBeInTheDocument();
 });
